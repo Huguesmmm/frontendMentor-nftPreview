f8375d3ecea566ab1a9f8f4e87d1bd9f248a38e2
Huguesmmm <hugues.prudhomme123@gmail.com>
2022-02-13 19:26:47 -0500

    Style folder organised with Sass bg change with blue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      
+
     </div>
     // <div className={styles.container}>
     //   <Head>
