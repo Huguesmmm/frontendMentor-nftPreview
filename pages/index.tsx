307bb322f71291f6e14c4cd4963a00b6c24dbd8a
Huguesmmm <hugues.prudhomme123@gmail.com>
2022-02-20 00:12:02 -0500

    hover effect added

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,12 @@ const Home: NextPage = () => {
 
     <div className="min-h-screen py-6 flex flex-col justify-center relative overflow-hidden ">
       <div className="card relative w-96 px-6 py-6 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:rounded-lg">
-        <Image src="/images/image-equilibrium.jpg" alt="Equilibrium" height={1000} width={1000} className='sm:rounded-lg'/>
+        <div className='img-nft'>
+          <Image src="/images/image-equilibrium.jpg" alt="Equilibrium" height={1000} width={1000} className='image' />
+          <div className='img-nft-hover'>
+            <img src="/icons/icon-view.svg" alt="eye"/>
+          </div>
+        </div>
         <div className='card-h1'>Equilibrium #3429</div>
         <div className='card-p'>Our Equilibrium collection promotes balance and calm.</div>
         <div className='card-info'>
