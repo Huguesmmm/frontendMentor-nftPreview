b3c11b9226dec0ec9b6c974bbba4964d2a66967f
Huguesmmm <hugues.prudhomme123@gmail.com>
2022-02-19 22:14:32 -0500

    added etherum and time status

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,10 @@ const Home: NextPage = () => {
         <Image src="/images/image-equilibrium.jpg" alt="Equilibrium" height={1000} width={1000} className='sm:rounded-lg'/>
         <div className='card-h1'>Equilibrium #3429</div>
         <div className='card-p'>Our Equilibrium collection promotes balance and calm.</div>
+        <div className='card-info'>
+          <div className='card-info-bitcoin'><img src="/icons/icon-ethereum.svg" alt="eth" /><p> 0.041 ETH</p></div>
+          <div className='card-info-time'><img src="/icons/icon-clock.svg" alt="eth" /><p>3 days left</p></div>
+        </div>
       </div>
     </div>
   )
