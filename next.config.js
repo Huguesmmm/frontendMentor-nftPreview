b0fd049cddf75c90a92bff0d21ef081ae7fbd0ec
Huguesmmm <hugues.prudhomme123@gmail.com>
2022-05-06 15:23:11 -0400

    try fix no images

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const nextConfig = {
   images: {
     loader: 'akamai',
-    path: '',
+    path: '/',
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
