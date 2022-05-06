c5cf210c062be34fb9088c18d5c07b1ff1a2ed35
Huguesmmm <hugues.prudhomme123@gmail.com>
2022-05-06 14:48:49 -0400

    correction of image optimization using akamai

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-const optimizedImages = require('next-optimized-images');
 const path = require('path');
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   },
