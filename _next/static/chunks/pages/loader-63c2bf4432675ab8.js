1984a99345172d503da1ed598040d063bc34178b
Huguesmmm <hugues.prudhomme123@gmail.com>
2022-05-06 14:51:34 -0400

    Updates

---
+++ _next/static/chunks/pages/loader-63c2bf4432675ab8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[644],{9666:function(n,u,r){(window.__NEXT_P=window.__NEXT_P||[]).push(["/loader",function(){return r(1765)}])},1765:function(n){"use strict";n.exports=function(n){var u=n.src;return"/images/".concat(u)}}},function(n){n.O(0,[774,888,179],(function(){return u=9666,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
